ec7e04ce464059eae91f3b12e873f00836723da5
mpromonet <michel.promonet@free.fr>
2012-09-21 22:05:45 +0000

    update mcp23008 using i2c API instead of smbus

--- mcp23008_i2c.py
+++ mcp23008_i2c.py
@@ -5,11 +5,11 @@ import time
 bus = smbus.SMBus(0)
  
 # configure GPIO all outputs
-bus.write_byte_data(0x20,0x00,0x00)
+bus.write_i2c_block_data(0x20,0x00,[0x00])
 
 # set all bits to 1
 while 1:
-	bus.write_byte_data(0x20,0x09,0xFF)
+	bus.write_i2c_block_data(0x20,0x09,[0x55])
 	time.sleep(0.25)
-	bus.write_byte_data(0x20,0x09,0x0)
+	bus.write_i2c_block_data(0x20,0x09,[0xAA])
 	time.sleep(0.25)
