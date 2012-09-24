064a92dca7dab58a13e344bb174c2b6f3e80fb91
mpromonet <michel.promonet@free.fr>
2012-09-24 21:04:45 +0000

    mcp23008 use variable to store chip adress

--- mcp23008_i2c.py
+++ mcp23008_i2c.py
@@ -2,14 +2,15 @@
 import smbus
 import time
  
+I2CADDR=0x20
 bus = smbus.SMBus(0)
  
 # configure GPIO all outputs
-bus.write_i2c_block_data(0x20,0x00,[0x00])
+bus.write_i2c_block_data(I2CADDR,0x00,[0x00])
 
 # set all bits to 1
 while 1:
-	bus.write_i2c_block_data(0x20,0x09,[0x55])
+	bus.write_i2c_block_data(I2CADDR,0x09,[0x55])
 	time.sleep(0.25)
-	bus.write_i2c_block_data(0x20,0x09,[0xAA])
+	bus.write_i2c_block_data(I2CADDR,0x09,[0xAA])
 	time.sleep(0.25)
