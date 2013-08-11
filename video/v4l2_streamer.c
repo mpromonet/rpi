765f558963a29c0a067b769f54298539ef06e240
MPR <michel.promonet@free.fr>
2013-08-11 20:05:47 +0200

    avoid exit in the middle, print reading time

--- video/v4l2_streamer.c
+++ video/v4l2_streamer.c
@@ -10,6 +10,8 @@
 #include <sys/time.h>
 #include <sys/mman.h>
 #include <linux/videodev2.h>
+#include <sys/timeb.h>
+
 #include <libv4l2.h>
 
 #include <string>
@@ -38,23 +40,32 @@ class V4L2DeviceSource: public FramedSource
 		static V4L2DeviceSource* createNew(UsageEnvironment& env, V4L2DeviceParameters params) { return new V4L2DeviceSource(env, params); }
 
 	protected:
-		V4L2DeviceSource(UsageEnvironment& env, V4L2DeviceParameters params) : FramedSource(env), fParams(params), m_fd(-1), m_bufferSize(0)
+		V4L2DeviceSource(UsageEnvironment& env, V4L2DeviceParameters params) : FramedSource(env), m_params(params), m_fd(-1), m_bufferSize(0)
 		{
-			initdevice(fParams.m_devName.c_str());
-			envir().taskScheduler().turnOnBackgroundReadHandling( m_fd, V4L2DeviceSource::incomingPacketHandlerStub,this);		
+			m_fd = initdevice(m_params.m_devName.c_str());
+			if (m_fd == -1)
+			{
+				fprintf(stderr, "Init device:%s failure\n", m_params.m_devName.c_str());
+			}
+			else
+			{
+				envir().taskScheduler().turnOnBackgroundReadHandling( m_fd, V4L2DeviceSource::incomingPacketHandlerStub,this);		
+			}
 		}
+		
 		virtual ~V4L2DeviceSource()
 		{
 			v4l2_close(m_fd);
 		}
 
 	private:
-		static int xioctl(int fh, int request, void *arg)
+		static int xioctl(int fd, int request, void *arg)
 		{
 			int r = -1;
 
-			do {
-				r = v4l2_ioctl(fh, request, arg);
+			do 
+			{
+				r = v4l2_ioctl(fd, request, arg);
 			} while (r == -1 && ((errno == EINTR) || (errno == EAGAIN)));
 
 			return r;
@@ -64,73 +75,84 @@ class V4L2DeviceSource: public FramedSource
 		{
 			struct v4l2_format              fmt;
 
-			m_fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
-			if (m_fd < 0) {
+			int fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
+			if (fd < 0) 
+			{
 				perror("Cannot open device");
-				exit(EXIT_FAILURE);
+				return -1;
 			}
 
 			
 			// ===== CAPAPILITIES
 			struct v4l2_capability cap;
 			memset(&(cap), 0, sizeof(cap));
-			if (-1 == xioctl(m_fd, VIDIOC_QUERYCAP, &cap)) {
+			if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &cap)) 
+			{
 				fprintf(stderr, "xioctl cannot get capabilities error %d, %s\n", errno, strerror(errno));
-				exit(EXIT_FAILURE);
+				return -1;
 			}
 			fprintf(stderr, "driver:%s capabilities;%X\n", cap.driver, cap.capabilities);
 
-			if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+			if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) 
+			{
 				fprintf(stderr, "%s is no video capture device\n", dev_name);
-				exit(EXIT_FAILURE);
+				return -1;
 			}
-			if ((cap.capabilities & V4L2_CAP_READWRITE)) {
-					fprintf(stderr, "%s support read i/o\n", dev_name);
+			if ((cap.capabilities & V4L2_CAP_READWRITE))
+			{
+				fprintf(stderr, "%s support read i/o\n", dev_name);
 			}
-			if ((cap.capabilities & V4L2_CAP_STREAMING)) {
-					fprintf(stderr, "%s support streaming i/o\n", dev_name);
+			if ((cap.capabilities & V4L2_CAP_STREAMING)) 
+			{
+				fprintf(stderr, "%s support streaming i/o\n", dev_name);
 			}	
 			
 			// ===== FORMAT
 			memset(&(fmt), 0, sizeof(fmt));
 			fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-			fmt.fmt.pix.width       = fParams.m_width;
-			fmt.fmt.pix.height      = fParams.m_height;
-			fmt.fmt.pix.pixelformat = fParams.m_format;
+			fmt.fmt.pix.width       = m_params.m_width;
+			fmt.fmt.pix.height      = m_params.m_height;
+			fmt.fmt.pix.pixelformat = m_params.m_format;
 			fmt.fmt.pix.field       = V4L2_FIELD_ANY;
 			
-			if (xioctl(m_fd, VIDIOC_S_FMT, &fmt) == -1)
+			if (xioctl(fd, VIDIOC_S_FMT, &fmt) == -1)
 			{
 				fprintf(stderr, "xioctl cannot set format error %d, %s\n", errno, strerror(errno));
-				exit(EXIT_FAILURE);
+				return -1;
 			}
-			
-			if (fmt.fmt.pix.pixelformat != fParams.m_format) {
-				printf("Libv4l didn't accept format (%d). Can't proceed.\n", fParams.m_format);
-				exit(EXIT_FAILURE);
+			if (fmt.fmt.pix.pixelformat != m_params.m_format) 
+			{
+				printf("Libv4l didn't accept format (%d). Can't proceed.\n", m_params.m_format);
+				return -1;
 			}
-			if ((fmt.fmt.pix.width != fParams.m_width) || (fmt.fmt.pix.height != fParams.m_height))
+			if ((fmt.fmt.pix.width != m_params.m_width) || (fmt.fmt.pix.height != m_params.m_height))
+			{
 				printf("Warning: driver is sending image at %dx%d\n", fmt.fmt.pix.width, fmt.fmt.pix.width);
+			}
 
+			m_bufferSize =  fmt.fmt.pix.sizeimage;
 			
-			m_bufferSize =  (fmt.fmt.pix.width * fmt.fmt.pix.width);
-			return m_fd;
+			return fd;
 		}
 		
 		virtual void doGetNextFrame()
 		{
-			envir() << "V4L2DeviceSource::doGetNextFrame fd:"  << m_fd << " size:" << m_bufferSize << "\n";		
 		}
+		
 		static void incomingPacketHandlerStub(void* clientData, int mask)
 		{
 			V4L2DeviceSource* source = (V4L2DeviceSource*) clientData;
 			source->incomingPacketHandler(mask);
 		}	
+		
 		void incomingPacketHandler(int mask) 
 		{
 			if (!isCurrentlyAwaitingData()) return;
 			
 			char buffer[m_bufferSize];
+			
+			struct timeb ref;
+			ftime(&ref); 
 			int newFrameSize = v4l2_read(m_fd, &buffer,  m_bufferSize);
 			
 			if (newFrameSize < 0)
@@ -139,7 +161,13 @@ class V4L2DeviceSource: public FramedSource
 			}
 			else
 			{
-				envir() << "V4L2DeviceSource::incomingPacketHandler fd:"  << m_fd << " buffersize:" << m_bufferSize << " got size:"  << newFrameSize << "/" << fMaxSize<< "\n";		
+				struct timeb current;
+				ftime(&current); 
+				envir() << "V4L2DeviceSource::incomingPacketHandler read time:"  << int((current.time-ref.time)*1000 + current.millitm-ref.millitm) << " ms \n";		
+				
+				gettimeofday(&fPresentationTime, NULL);
+				fDurationInMicroseconds = 0;
+												
 				if (newFrameSize > fMaxSize) 
 				{
 					fFrameSize = fMaxSize;
@@ -149,7 +177,6 @@ class V4L2DeviceSource: public FramedSource
 				{
 					fFrameSize = newFrameSize;
 				}
-				gettimeofday(&fPresentationTime, NULL);
 				memcpy(fTo, &buffer, fFrameSize);
 	  
 				afterGetting(this); 
@@ -157,7 +184,7 @@ class V4L2DeviceSource: public FramedSource
 		}	
 
 	private:
-		V4L2DeviceParameters fParams;
+		V4L2DeviceParameters m_params;
 		int m_fd;
 		int m_bufferSize;
 };
@@ -222,8 +249,9 @@ class MJPEGVideoSource : public JPEGVideoSource
 
 			if (headerOk)
 			{
-				envir() << "MJPEGVideoSource::afterGettingFrame time:" << (presentationTime.tv_sec*1000000.0+presentationTime.tv_usec)/1000000.0 << " duration:"<< durationInMicroseconds  << " size:" << (m_width*8) << "x" << (m_height*8) <<"\n";
-
+				envir() << "MJPEGVideoSource::afterGettingFrame time:" << (presentationTime.tv_sec*1000000.0+presentationTime.tv_usec)/1000000.0 
+					<< " m_qTable0Init:" << m_qTable0Init << " m_qTable1Init:" << m_qTable1Init				
+					<< " size:" << (m_width*8) << "x" << (m_height*8) << "\n";
 				fFrameSize = frameSize - headerSize;
 				memmove( fTo, fTo + headerSize, fFrameSize );
 			}
@@ -243,7 +271,7 @@ class MJPEGVideoSource : public JPEGVideoSource
 		virtual u_int8_t qFactor() { return 128; };
 		virtual u_int8_t width() { return m_width; };
 		virtual u_int8_t height() { return m_height; };
-		u_int8_t const* quantizationTables( u_int8_t& precision, u_int16_t& length )
+		virtual u_int8_t const* quantizationTables( u_int8_t& precision, u_int16_t& length )
 		{
 			length = 0;
 			precision = 0;
@@ -275,14 +303,6 @@ class MJPEGVideoSource : public JPEGVideoSource
 		bool          m_qTable1Init;
 };
 
-void afterPlaying(void* clientData) 
-{
-	RTPSink* videoSink = (RTPSink*)clientData;
-	videoSink->envir() << "...done reading from file\n";
-	videoSink->stopPlaying();
-}
-
-
 char quit = 0;
 void sighandler(int n)
 { 
@@ -296,70 +316,76 @@ int main(int argc, char** argv)
 	TaskScheduler* scheduler = BasicTaskScheduler::createNew();
 	UsageEnvironment* env = BasicUsageEnvironment::createNew(*scheduler);
 
-	// Create 'groupsocks' for RTP and RTCP:
-	struct in_addr destinationAddress;
-	destinationAddress.s_addr = chooseRandomIPv4SSMAddress(*env);
-
-	const unsigned short rtpPortNum = 18888;
-	const unsigned short rtcpPortNum = rtpPortNum+1;
-	const unsigned char ttl = 255;
-
-	const Port rtpPort(rtpPortNum);
-	const Port rtcpPort(rtcpPortNum);
-
-	Groupsock rtpGroupsock(*env, destinationAddress, rtpPort, ttl);
-	Groupsock rtcpGroupsock(*env, destinationAddress, rtcpPort, ttl);
-
-	// Create a 'Video RTP' sink from the RTP 'groupsock':
-	OutPacketBuffer::maxSize = 1000000;
-	RTPSink* videoSink = JPEGVideoRTPSink::createNew(*env, &rtpGroupsock);
-
-	// Create (and start) a 'RTCP instance' for this RTP sink:
-	const unsigned estimatedSessionBandwidth = 500; // in kbps; for RTCP b/w share
-	const unsigned maxCNAMElen = 100;
-	unsigned char CNAME[maxCNAMElen+1];
-	gethostname((char*)CNAME, maxCNAMElen);
-	CNAME[maxCNAMElen] = '\0'; // just in case
-	RTCPInstance* rtcp = RTCPInstance::createNew(*env, &rtcpGroupsock,  estimatedSessionBandwidth, CNAME, videoSink, NULL /* we're a server */);
-
 	RTSPServer* rtspServer = RTSPServer::createNew(*env, 8554);
 	if (rtspServer == NULL) 
 	{
 		*env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
-		exit(1);
 	}
-	ServerMediaSession* sms = ServerMediaSession::createNew(*env, "testStream", "", "Session streamed by \"testH264VideoStreamer\"");
-	sms->addSubsession(PassiveServerMediaSubsession::createNew(*videoSink, rtcp));
-	rtspServer->addServerMediaSession(sms);
+	else
+	{		
+		// Create RTP and RTCP groupsock:
+		struct in_addr destinationAddress;
+		destinationAddress.s_addr = chooseRandomIPv4SSMAddress(*env);
 
-	char* url = rtspServer->rtspURL(sms);
-	*env << "Play this stream using the URL \"" << url << "\"\n";
-	delete[] url;
+		const unsigned short rtpPortNum = 18888;
+		const unsigned short rtcpPortNum = rtpPortNum+1;
+		const unsigned char ttl = 255;
 
-	// Start the streaming:
-	*env << "Create V4L2 Source...\n";
-        char                            *dev_name = "/dev/video0";	
-	if (argc ==2) dev_name=argv[1];
-	V4L2DeviceSource::V4L2DeviceParameters param(dev_name);
-	V4L2DeviceSource* videoES = V4L2DeviceSource::createNew(*env, param);
-	if (videoES == NULL) {
-		*env << "Unable to create source";
-		exit(1);
+		const Port rtpPort(rtpPortNum);
+		const Port rtcpPort(rtcpPortNum);
+
+		Groupsock rtpGroupsock(*env, destinationAddress, rtpPort, ttl);
+		Groupsock rtcpGroupsock(*env, destinationAddress, rtcpPort, ttl);
+
+		// Create a 'Video RTP' sink from the RTP 'groupsock':
+		OutPacketBuffer::maxSize = 3000000;
+		RTPSink* videoSink = JPEGVideoRTPSink::createNew(*env, &rtpGroupsock);
+
+		// Create 'RTCP instance' for this RTP sink:
+		const unsigned estimatedSessionBandwidth = 500; // in kbps; for RTCP b/w share
+		const unsigned maxCNAMElen = 100;
+		unsigned char CNAME[maxCNAMElen+1];
+		gethostname((char*)CNAME, maxCNAMElen);
+		CNAME[maxCNAMElen] = '\0'; 
+		RTCPInstance* rtcp = RTCPInstance::createNew(*env, &rtcpGroupsock,  estimatedSessionBandwidth, CNAME, videoSink, NULL);
+		
+		// Create Server Session
+		ServerMediaSession* sms = ServerMediaSession::createNew(*env);
+		sms->addSubsession(PassiveServerMediaSubsession::createNew(*videoSink, rtcp));
+		rtspServer->addServerMediaSession(sms);
+
+		char* url = rtspServer->rtspURL(sms);
+		*env << "Play this stream using the URL \"" << url << "\"\n";
+		delete[] url;
+
+		// Start the streaming:
+		char                            *dev_name = "/dev/video0";	
+		if (argc ==2) dev_name=argv[1];
+		*env << "Create V4L2 Source..." << dev_name << "\n";
+		V4L2DeviceSource::V4L2DeviceParameters param(dev_name);
+		V4L2DeviceSource* videoES = V4L2DeviceSource::createNew(*env, param);
+		if (videoES == NULL) 
+		{
+			*env << "Unable to create source";
+			exit(1);
+		}
+
+		// Create a framer for the Video Elementary Stream:
+		*env << "Create MJPEG Source...\n";
+		MJPEGVideoSource* videoSource = MJPEGVideoSource::createNew(*env, videoES);
+
+		// Finally, start playing:
+		*env << "Starting...\n";
+		videoSink->startPlaying(*videoSource, NULL, videoSink);
+
+		signal(SIGINT,sighandler);
+		env->taskScheduler().doEventLoop(&quit); 
+		*env << "Exiting..\n";
+
+		Medium::close(videoSink);
+		Medium::close(rtspServer);
 	}
-
-	// Create a framer for the Video Elementary Stream:
-	*env << "Create MJPEG Source...\n";
-	MJPEGVideoSource* videoSource = MJPEGVideoSource::createNew(*env, videoES);
-
-	// Finally, start playing:
-	*env << "Starting...\n";
-	videoSink->startPlaying(*videoSource, afterPlaying, videoSink);
-
-	signal(SIGINT,sighandler);
-	env->taskScheduler().doEventLoop(&quit); 
-
-	Medium::close(videoSink);
-	Medium::close(rtspServer);
+	
 	env->reclaim();
 	delete scheduler;
 	
